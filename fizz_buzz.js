14d47673793e6e374aca7080b08a54ee46396d76
WorkingAnt <piyushb9@gmail.com>
2018-12-11 14:12:32 +0530

    version 2 fizz buzz

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -9,7 +9,9 @@
  */
 for(let index=1;index<=100;index++)
 {
-	if(index%3===0)
+	if(index%3 === 0 && index%5 === 0)
+		console.log("Fizz Buzz")
+	else if(index%3===0)
 		console.log("Fizz");
 	else if(index%5===0)
 		console.log("Buzz");
