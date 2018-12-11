d6fd6ce665666fb5433b9b6ad730a0cf14a7582f
WorkingAnt <piyushb9@gmail.com>
2018-12-11 14:10:11 +0530

    version 1 for fizz buzz

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -1,11 +1,18 @@
+/**
+ * Problem statement
+ * version 1
+ * Print all numbers from 1 to 100
+ * print Fizz for numbers divisible by 3
+ * print Buzz for numbers divisible by 5 and not by 3
+ * version 2 = version 1 + ...
+ * print FizzBuzz for numbers divisible by 5 and 3
+ */
 for(let index=1;index<=100;index++)
 {
-	if(index%5===0 && index%3===0)
-		console.log("FizzBuzz");
-	else if(index%5 === 0)
+	if(index%3===0)
+		console.log("Fizz");
+	else if(index%5===0)
 		console.log("Buzz");
-	else if(index%3 === 0)
-		console.log("Fizz")
 	else
 		console.log(index);
 }
