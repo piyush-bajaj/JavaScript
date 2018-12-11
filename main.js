f806602b6f8b1bf981d441caefa67640e949f160
WorkingAnt <piyushb9@gmail.com>
2018-12-11 14:03:14 +0530

    adding multiple solutions

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
+/*
+ * Problem statement
+ * Print the pattern 
+ *	#
+ *	##
+ *	###
+ *	####
+ *	#####
+ *	######
+ *	#######
+ */
+/**
+ * Solution 1
+ */
 let output = "#######";
 for(let index=1;index<=7;index++)
 {
 	console.log(output.substring(0,index));
+}
+/**
+ * Solution 2
+ */
+for(let out="#";out.length<8;out+="#")
+{
+	console.log(out);
 }
