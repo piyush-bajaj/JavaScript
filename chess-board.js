0892a65c2931e9bf3beaea70b4d3f705f49e0539
WorkingAnt <piyushb9@gmail.com>
2018-12-11 14:43:51 +0530

    chess board version 2

--- chess-board.js
+++ chess-board.js
@@ -1,15 +1,17 @@
 /**Problem statement
  * Create a string that represents 8X8 grid using newline character
  * grid contains space( ) or hash(#)
+ * now create a variable to make the size of the board dynamic
  * 
  *  */
 let pattern1 = " ";
 let pattern2 = "#";
 let grid = "";
+let size = 8;
 
-for(let i=0;i<8;i++)
+for(let i=0;i<size;i++)
 {
-	for(let j=0;j<8;j++)
+	for(let j=0;j<size;j++)
 	{
 		if((i%2===0 && j%2===0) || (i%2!==0 && j%2!==0))
 			grid+=pattern1;
