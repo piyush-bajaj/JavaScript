ca65556d6208cc8ac78e415dbfacdd5c5e29e1a2
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2018-12-16 18:37:32 +0530

    adding comments bean counting

--- bean-counting.js
+++ bean-counting.js
@@ -1,7 +1,19 @@
+/**
+ * function that returns the number of B's in the given text
+ * it uses the function countChar()
+ * 
+ * @param {String} text 
+ */
 function countBs(text){
 	return countChar(text,"B");
 }
 
+/**
+ * function that returns the number if characters in the given text
+ * 
+ * @param {String} text 
+ * @param {String} char 
+ */
 function countChar(text, char){
 	let count = 0;
 	for(let i = 0;i < text.length;i++){
