5424346e6f4189176018666297579d9d343962be
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2018-12-16 18:15:03 +0530

    verison 2 recursion

--- recursion.js
+++ recursion.js
@@ -1,10 +1,12 @@
 /**
  * function that returns true if number is even else it returns false
- * 
+ * return undefined if number is negative
  * @param {Number} number 
  */
 function isEven(number)
 {
+	if(number < 0)
+		return;
 	if(number === 0)
 		return true;
 	else if(number === 1)
