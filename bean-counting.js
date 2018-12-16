28e721ea851eab5210716cb261f751c880f52530
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2018-12-16 18:27:54 +0530

    bean counting  version2

--- bean-counting.js
+++ bean-counting.js
@@ -1,10 +1,19 @@
 function countBs(text){
 	let count = 0;
-	for(let i=0;i<text.length;i++){
+	for(let i = 0;i < text.length;i++){
 		if(text[i] === "B")
 			count++;
 	}
 	return count;
 }
 
+function countChar(text, char){
+	let count = 0;
+	for(let i = 0;i < text.length;i++){
+		if(text[i] === char)
+			count++;
+	}
+	return count;
+}
 console.log(countBs("hjfsdaBdsfBsdf dsafBsadfB"));
+console.log(countChar("dsfdRvdsfBSDFSDFsdfSDFASsdf","d"));
